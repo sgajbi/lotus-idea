5f835130e086ebd95dcc25beb6a7ab60105defd5
sgajbi <sandeepgajbi@yahoo.com>
2026-09-05 11:41:48 +0800

    fix(certification): track trusted-time queue proof (#1226)

--- src/app/application/durable_repository_proof/ci_receipt.py
+++ src/app/application/durable_repository_proof/ci_receipt.py
@@ -29,7 +29,7 @@ _GOVERNED_POSTGRES_TESTS = (
     ),
     (
         "tests.integration.test_postgres_review_queue_runtime",
-        "test_postgres_review_queue_preserves_snapshot_across_future_insert_and_rejects_stale_token",
+        "test_postgres_review_queue_preserves_historical_snapshot_across_later_accepted_insert",
     ),
 )
 
