17592dee9003b613d9a5865cff3d485a75a27cd8
sgajbi <sandeepgajbi@yahoo.com>
2026-09-05 10:48:29 +0800

    test(time): anchor critical workflow control time (#1226)

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,10 +1,22 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
+from datetime import UTC, datetime
 
 import pytest
 
+from app.runtime import trusted_clock_state
+from tests.support.fixed_utc_clock import FixedUtcClock
 from tests.support.http import managed_test_client_scope
+
+
+@pytest.fixture(autouse=True)
+def _deterministic_control_time(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        trusted_clock_state,
+        "_TRUSTED_CLOCK",
+        FixedUtcClock(datetime(2026, 6, 21, 10, 10, tzinfo=UTC)),
+    )
 
 
 @pytest.fixture(autouse=True)
