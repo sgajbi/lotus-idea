234079ac457e183aae52a4a3a0b783473fcdff83
sgajbi <sandeepgajbi@yahoo.com>
2026-09-05 17:24:04 +0800

    test(migration): include presentation revision binding

--- tests/unit/test_migration_execution.py
+++ tests/unit/test_migration_execution.py
@@ -131,8 +131,9 @@ def test_discover_migrations_requires_rollbacks() -> None:
         "022",
         "023",
         "024",
-        "025",
-    ]
+            "025",
+            "026",
+        ]
     assert migrations[0].rollback_path.name == "001_idea_repository_foundation.rollback.sql"
     assert migrations[1].rollback_path.name == "002_ai_explanation_lineage.rollback.sql"
     assert migrations[2].rollback_path.name == "003_outbox_event_contract_constraints.rollback.sql"
